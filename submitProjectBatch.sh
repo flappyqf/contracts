4bdbef044a271e8a80737256f1d2488930dc72c4
Yudhishthra Sugumaran <66850474+0xYudhishthra@users.noreply.github.com>
2024-08-24 20:53:37 +0900

    chore: Update submitProjectBatch.sh script to include project acceptance and matching initiation

--- submitProjectBatch.sh
+++ submitProjectBatch.sh
@@ -2,12 +2,22 @@
 
 source .env
 
-CONTRACT_ADDRESS="0x4A0AB940534DF591dD27e374cC90B46666e05911"
+CONTRACT_ADDRESS="0x0BaA3846bB2793e17f72a17A96916b5F26953e0e"
 
-for i in {1..8}
+for i in {0..7}
 do
     IPFS_HASH="QmHash${i}"  # Replace with actual IPFS hashes
     cast send $CONTRACT_ADDRESS "submitProject(string)" "test" --rpc-url $SEPOLIA_RPC_URL --private-key $PRIV_KEY
     echo "Submitted project $i"
     sleep 5  # Wait 5 seconds between transactions
 done
+
+for i in {0..7}
+do
+    cast send $CONTRACT_ADDRESS "acceptProject(uint256)" $i --rpc-url $SEPOLIA_RPC_URL --private-key $PRIV_KEY
+    echo "Accepted project $i"
+    sleep 5  # Wait 5 seconds between transactions
+done
+    
+
+cast send $CONTRACT_ADDRESS "initiateMatching()" --rpc-url $SEPOLIA_RPC_URL --private-key $PRIV_KEY
